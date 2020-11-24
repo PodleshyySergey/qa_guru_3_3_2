888979ef219fba32505964c988992db8cc0758b7
PodleshyySergey <60898756+PodleshyySergey@users.noreply.github.com>
2020-11-24 12:28:02 +0300

    Все тесты добавил в один в класс. Добавил порядок выполнения тестов.

---
+++ src/test/java/depositsAndInsuranceTests.java
@@ -0,0 +1,61 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.*;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class depositsAndInsuranceTests {
+
+    @BeforeEach
+    public void setUp() {
+        Configuration.browser = "chrome";
+        Configuration.startMaximized = true;
+        Selenide.open("https://alfabank.ru/make-money/");
+    }
+
+    @Test
+    @Order(0)
+    public void depositTest() {
+        $x("//h1[contains(text(),'Накопительные продукты')]").shouldBe(Condition.text("Накопительные продукты"));
+        $x("//p[contains(text(),'Депозиты')]/..").click();
+        $x("//button[contains(text(),'Архивные счета и депозиты')]").click();
+        $x("//span[contains(text(),'Депозиты')]").click();
+        $$x("//div[@data-widget-name='CatalogCard']").find(Condition.visible).getSize().equals(5);
+    }
+
+    @Test
+    @Order(1)
+    public void siblingTest() {
+        $("[title=Вклады]").parent().click();
+        $(byText("Описание")).parent().sibling(0).click();
+        $(byText("Полезно знать")).parent().scrollTo().shouldBe(Condition.text("Страхованию подлежат"));
+    }
+
+    @Test
+    @Order(2)
+    public void precedingTest() {
+        $("[title=Вклады]").parent().click(); //Что такое вклад?
+        $(byText("Что такое вклад?")).parent().preceding(0).click();
+        $(byText("Полезно знать")).parent().scrollTo().shouldBe(Condition.text("Страхованию подлежат"));
+    }
+
+    @Test
+    @Order(3)
+    public void parentTest() {
+        $("[title=Вклады]").parent().click();
+        $(byText("Страхование вкладов")).parent().click();
+        $(byText("Полезно знать")).parent().scrollTo().shouldBe(Condition.text("Страхованию подлежат"));
+    }
+
+    @Test
+    @Order(4)
+    public void closestTest() {
+        $("[title=Вклады]").parent().click();
+        $(byText("Страхование вкладов")).closest("button").click();
+        $(byText("Полезно знать")).parent().scrollTo().shouldBe(Condition.text("Страхованию подлежат"));
+    }
+
+}
